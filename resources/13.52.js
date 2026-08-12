2b973f7ef20aa3920ed70bf3cfdf4f90cd003953
Leandrobts <kazaalite_601@hotmail.com>
2026-08-12 16:08:53 -0300

    Add files via upload

---
+++ resources/13.52.js
@@ -0,0 +1,10 @@
+// 13.52 -- generated from libSceNKWebKit / libkernel_web /
+// libSceLibcInternal. file offset = rva + 0x4000
+
+// host-constructor candidates: webkitBase = nativeCtorAddr - hc
+const OFFSET_wk_host_constructor_candidates = [0x00015060, 0x00015120, 0x000151C0, 0x000152C0, 0x000153E0];
+// Exact WKDownloadGetTypeID export (NID -x5vK4NNNYM).
+
+const OFFSET_lk_getpid                         = 0x00010E4D;
+
+const OFFSET_lc_memset                         = 0x000D3439;
